025a07b957ddb4333fe5dcca5aabd79eb8a5bcad
Franz-Kim <71546458+Franz-Kim@users.noreply.github.com>
2021-05-09 16:31:49 +0900

    cpu_

--- run_image2emotion.py
+++ run_image2emotion.py
@@ -12,12 +12,13 @@ img_dim=224 # where can i find?
 
 ARTEMIS_EMOTIONS = ['amusement', 'awe', 'contentment', 'excitement',
                     'anger', 'disgust',  'fear', 'sadness', 'something else']
-
+"""
 def get_freer_gpu():
     os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
     memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
     
     return np.argmax(memory_available)
+"""
 
 # set transformation
 resample_method = Image.LANCZOS
@@ -34,7 +35,7 @@ img = img_transform(img)
 
 # get model
 pretrained_model_path='image2emotion_pretrained.pt'
-gpuIdx = get_freer_gpu()
+#gpuIdx = get_freer_gpu()
 #device = torch.device("cuda:" + str(gpuIdx))
 device = torch.device("cpu")
 
@@ -54,7 +55,8 @@ with torch.no_grad():
 
 
 # print info
-import pdb
-pdb.set_trace()
 pred_info = {ARTEMIS_EMOTIONS[l]:pred[l].item() for l in range(pred.size(-1))}
 print(pred_info)
+
+best_prediction_idx = pred.argmax().item()
+print('highest emotion = ' + str(ARTEMIS_EMOTIONS[best_prediction_idx]))
