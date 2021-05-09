a3c6a138a249f7edd52d956c45ef6d4072ebce25
developer-account <gm.k@navercorp.com>
2021-05-09 17:33:38 +0900

    onnx runnable

--- run_image2emotion.py
+++ run_image2emotion.py
@@ -9,6 +9,7 @@ import numpy as np
 image_net_mean = [0.485, 0.456, 0.406]
 image_net_std = [0.229, 0.224, 0.225]
 img_dim=224 # where can i find?
+batch_size = 1
 
 ARTEMIS_EMOTIONS = ['amusement', 'awe', 'contentment', 'excitement',
                     'anger', 'disgust',  'fear', 'sadness', 'something else']
@@ -35,6 +36,7 @@ img = img_transform(img)
 
 # get model
 pretrained_model_path='image2emotion_pretrained.pt'
+#pretrained_model_path='pt16_model.pt'
 #gpuIdx = get_freer_gpu()
 #device = torch.device("cuda:" + str(gpuIdx))
 device = torch.device("cpu")
@@ -60,3 +62,25 @@ print(pred_info)
 
 best_prediction_idx = pred.argmax().item()
 print('highest emotion = ' + str(ARTEMIS_EMOTIONS[best_prediction_idx]))
+
+
+# convert to onnx
+#x = torch.randn(batch_size, 3, img_dim, img_dim)
+print('convert to onnx') # works on pytorch < 1.6
+torch.onnx.export(model,
+                img,
+                "saved_model_onnx.onnx",
+                export_params=True,
+                opset_version=10,
+                do_constant_folding=True,
+                input_names = ['input'],
+                output_names = ['output'],
+                dynamic_axes={'input':{0:'batch_size'},
+                            'output':{0: 'batch_size'}}
+                )
+
+# load onnx model & check output 
+import onnx 
+onnx_model = onnx.load("saved_model_onnx.onnx") 
+onnx.checker.check_model(onnx_model)
+
